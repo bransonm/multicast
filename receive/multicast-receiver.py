bfdc83f9213267ef4e2fac0d5861371ce5a08a43
bransonm <branson1@us.ibm.com>
2022-11-21 15:03:33 -0500

    Update multicast-receiver.py

--- receive/multicast-receiver.py
+++ receive/multicast-receiver.py
@@ -4,7 +4,7 @@ import sys
 
 multicast_group = '224.3.29.71'
 server_address = ('', 10000)
-
+print ("Hello from Receiver")
 # Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
